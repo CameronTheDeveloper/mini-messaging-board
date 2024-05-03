d8b2c61108dfa466a850447b8599c97b7923ed83
CameronTheDeveloper <Cameronposey@outlook.com>
2024-05-02 22:57:29 -0700

    Add messages data

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const messages = [
+  {
+    user: "Jason",
+    message: "Hello, I enjoy computers.",
+    when: new Date()
+  },
+  {
+    user: "Michelle",
+    message: "Hello World!",
+    when: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
