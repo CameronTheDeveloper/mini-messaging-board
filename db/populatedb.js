b23491788b5fbc4459527ce0f2c07fc7999b0ae0
CameronTheDeveloper <Cameronposey@outlook.com>
2024-09-03 01:27:33 -0700

    Add populatedb.js

---
+++ db/populatedb.js
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+require('dotenv').config();
+const { Client } = require('pg');
+
+const SQL = ` 
+CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(255),
+    message VARCHAR(255)
+);
+
+INSERT INTO messages (username, message)
+VALUES
+('Bryan', 'Hello, how are you doing today?'),
+('Sarah', 'I am doing good, how about yourself?'),
+('Odin', 'Nice to meet both of you!');
+`;
+
+async function main() {
+    console.log('seeding...');
+    const client = new Client({
+        connectionString: process.env.POSTGRESQL_CONNECTION_STRING
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log('done');
+}
+
+main();
