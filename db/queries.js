f98e5d9629dede2aee23f8c0cc3990b7a2ed3ba7
CameronTheDeveloper <Cameronposey@outlook.com>
2024-09-03 03:57:38 -0700

    Add deleteAllMessages query

--- db/queries.js
+++ db/queries.js
@@ -9,7 +9,12 @@ async function insertMessage(username, message) {
     await pool.query('INSERT INTO messages (username, message) VALUES ($1, $2)', [username, message]);
 }
 
+async function deleteAllMessages() {
+    await pool.query('DELETE FROM messages');
+}
+
 module.exports = {
     getAllMessages,
-    insertMessage
+    insertMessage,
+    deleteAllMessages
 };
